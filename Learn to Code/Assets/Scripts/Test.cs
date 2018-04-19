0a4af94604afd0ede5366c6ec528049aee69f551
Quillenator <32397666+Quillenator@users.noreply.github.com>
2018-04-18 22:29:02 -0400

    Test Update
    Played around with some new things in test script

--- Learn to Code/Assets/Scripts/Test.cs
+++ Learn to Code/Assets/Scripts/Test.cs
@@ -4,9 +4,34 @@ using UnityEngine;
 
 public class Test : MonoBehaviour {
 
+    //Properties
+    private int experience;
+
+    public int Experience
+    {
+        get { return experience; }
+        set { experience = value; }
+    }
+
+    public int Level
+    {
+        get { return experience / 100; }
+        set { experience = value * 100; }
+    }
+
+
+    public static int enemyCount = 0;
+
 	// Use this for initialization
 	void Start () {
-        Debug.Log("Test");
+
+        int health = 20;
+        string message;
+
+        //Ternary
+        message = health > 0 ? "Player is Alive" : "Player is Dead";
+
+        Debug.Log(message);
 	}
 	
 	// Update is called once per frame
